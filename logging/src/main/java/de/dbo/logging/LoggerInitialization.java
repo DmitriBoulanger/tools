aee5203d8550f94ca5d888a893aab6b3d26cf52e
Dmitri Boulanger <boulanger@ityx.de>
2014-10-07 11:26:24 +0200

    logging done and tested

--- logging/src/main/java/de/dbo/logging/LoggerInitialization.java
+++ logging/src/main/java/de/dbo/logging/LoggerInitialization.java
@@ -1,61 +1,111 @@
 package de.dbo.logging;
 
+import static uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J.restoreOriginalSystemOutputs;
+import static uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J.sendSystemOutAndErrToSLF4J;
+import static uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J.systemOutputsAreSLF4JPrintStreams;
+
 import java.io.File;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.Appender;
 import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Initialization of the Log4j-logger
- * 
+ *
  * @author Dmitri Boulanger, Hombach
  *
- * D. Knuth: Programs are meant to be read by humans and 
- *           only incidentally for computers to execute 
+ * D. Knuth: Programs are meant to be read by humans and
+ *           only incidentally for computers to execute
  *
  */
 public class LoggerInitialization {
 
-    public static final String LOGGER_CONFIG_RESOURCE_DEFAULT = "dbo.log4j.properties";
-    public static final String LOGGER_CONFIG_PROPERTY = "dbo.log4j.properties";
-    
-    /**
-     * initialization from resource
-     * @param resource
-     * @throws Exception if no resource found
+    public static final String LOGGER_CONFIG_RESOURCE_DEFAULT  = "dbo.log4j.properties";
+
+    /*
+     * system properties
      */
-    public static boolean initializeLogger(final String resource) throws Exception {
-    	 LogManager.resetConfiguration();
-    	 return doInitialize(resource);
+    public static final String LOGGER_CONFIG_PATH_PROPERTY     = "log4j.properties.path";
+    public static final String LOGGER_CONFIG_RESOURCE_PROPERTY = "log4j.properties.resource";
+
+    /**
+     *
+     * @return true the root-logger has appenders
+     */
+    public static boolean isAvailable() {
+        @SuppressWarnings("unchecked")
+        final List<Appender> appenders = Collections.list(LogManager.getRootLogger().getAllAppenders());
+        return !appenders.isEmpty();
     }
-    
+
+    /**
+     * all console-messages send to the relevant logger.
+     * The operation is only done if the root-logger has been already initialized
+     *
+     * @throws Exception
+     */
+    public static void outAndErrToLog(boolean yes) throws Exception {
+        if (yes && isAvailable()) {
+            sendSystemOutAndErrToSLF4J();
+            return;
+        }
+        if (!yes && systemOutputsAreSLF4JPrintStreams()) {
+            restoreOriginalSystemOutputs();
+        }
+    }
+
     /**
      * initialization from file-path in the system-properties.
      * If such a file is not found, the default resource is used
-     * 
+     *
      * @param resource
+     * @return false only if the logger has not been initialized
      * @throws Exception if no resource found
      */
-    public static void initializeLogger() throws Exception {
+    public static boolean initializeLogger() throws Exception {
     	boolean done = false;
         LogManager.resetConfiguration();
 
-        final String log4jPropertyPath = System.getProperty(LOGGER_CONFIG_PROPERTY);
-        if (log4jPropertyPath != null) {
-        	done = doInitialize( new File(log4jPropertyPath) );
+        final String log4jConfigurationFilePath = System.getProperty(LOGGER_CONFIG_PATH_PROPERTY);
+        final String log4jConfigurationResource = System.getProperty(LOGGER_CONFIG_RESOURCE_PROPERTY);
+        if (null != log4jConfigurationFilePath && 0 != log4jConfigurationFilePath.trim().length()) {
+            done = initializeFromFile(new File(log4jConfigurationFilePath));
+        }
+        else if (!done && null != log4jConfigurationResource && 0 != log4jConfigurationResource.trim().length()) {
+            done = initializeFromResource(log4jConfigurationResource);
         }
         if (done) {
-        	return;
+            return isAvailable();
         }
-        done = doInitialize( LOGGER_CONFIG_RESOURCE_DEFAULT );
-        
+
+        // no custom-configuration available. Initialization from the default resource ...
+        done = initializeFromResource(LOGGER_CONFIG_RESOURCE_DEFAULT);
         if (!done) {
-        	throw new Exception("No Logger configuration found!"); 
+            throw new Exception("Should never happen error: no default configuration-resource found!");
         }
+
+        return isAvailable();
     }
-    
-    private static boolean doInitialize(final File file)  {
+
+    /**
+     * Log4j-initialization from configuration in resource (classpath)
+     * @param resource
+     * @return false only if the logger has not been initialized
+     * @throws Exception if no resource found
+     */
+    public static boolean initializeLogger(final String resource) throws Exception {
+        LogManager.resetConfiguration();
+        if (!initializeFromResource(resource)) {
+            return false;
+        }
+        return isAvailable();
+    }
+
+    private static boolean initializeFromFile(final File file) {
     	if (null==file) {
     		return false;
     	}
@@ -68,8 +118,8 @@ public class LoggerInitialization {
     		return false;
     	}
     }
-    
-    private static boolean doInitialize(final String resource) throws Exception {
+
+    private static boolean initializeFromResource(final String resource) throws Exception {
     	final URL logConfig = LoggerInitialization.class.getClassLoader().getResource(resource);
         if (logConfig != null) {
              System.out.println("Initializing Log4j-Logger with resource-configuration [" + resource + "] ...");
--- logging/src/test/java/de/dbo/logging/LoggerInitializationTest.java
+++ logging/src/test/java/de/dbo/logging/LoggerInitializationTest.java
@@ -1,53 +1,168 @@
 package de.dbo.logging;
 
-import static de.dbo.logging.LoggerInitialization.LOGGER_CONFIG_PROPERTY;
+import static de.dbo.logging.LoggerInitialization.LOGGER_CONFIG_PATH_PROPERTY;
+import static de.dbo.logging.LoggerInitialization.LOGGER_CONFIG_RESOURCE_PROPERTY;
 import static de.dbo.logging.LoggerInitialization.initializeLogger;
+import static de.dbo.logging.LoggerInitialization.isAvailable;
+import static de.dbo.logging.LoggerInitialization.outAndErrToLog;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.apache.log4j.LogManager;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.*;
-
-import org.apache.log4j.LogManager;
-
 public class LoggerInitializationTest {
 	private static final Logger log = LoggerFactory.getLogger(LoggerInitializationTest.class);
-	
+
+    /**
+     * No parameters at all.
+     * Logger should be initialized from default configuration-resource
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDefaultInitialization() throws Exception {
+        clearSystemProperties();
+        assertTrue("Logger was not initialized usig default initialization", initializeLogger());
+        log.debug("Loggger initialized from the default resource");
+        assertFalse("Default resource-configuration has only INFO-prioriry for the de.dbo-loggers but the logger has isDebugEnabled=true"
+                , log.isDebugEnabled());
+    }
+
+    /**
+     * path of the configuration-file is given in the system-properties.
+     * Logger should be initialized from the given configuration-file
+     * @see LoggerInitialization#LOGGER_CONFIG_PATH_PROPERTY
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testIntializationFromGivenFile() throws Exception {
+        clearSystemProperties();
+        final String path = "src/test/resources/data/dbo2.log4j.properties";
+        System.setProperty(LOGGER_CONFIG_PATH_PROPERTY, path);
+        assertTrue("Logger was not initialized from configuration-file [" + path + "]", initializeLogger());
+        log.debug("Loggger initialized from custom configuration-file [" + path + "] with DEBUG-priority");
+        assertTrue("Test configuration [" + path + "] has DEBUG-prioriry for the de.dbo-loggers but the logger has isDebugEnabled=false"
+                , log.isDebugEnabled());
+    }
+
+    /**
+     * path of the configuration-resource is given in the system-properties.
+     * Logger should be initialized from the given configuration-resource
+     * @see LoggerInitialization#LOGGER_CONFIG_RESOURCE_PROPERTY
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testIntializationFromGivenResource() throws Exception {
+        clearSystemProperties();
+        final String resource = "data/dbo2.log4j.properties";
+        System.setProperty(LOGGER_CONFIG_RESOURCE_PROPERTY, resource);
+        assertTrue("Logger was not initialized from custom configuration-resource [" + resource + "]", initializeLogger());
+        log.debug("Loggger initialized from custom configuration-resource [" + resource + "] with DEBUG-priority");
+        assertTrue("Test configuration  [" + resource + "] has DEBUG-prioriry for the de.dbo-loggers but the logger has isDebugEnabled=false"
+                , log.isDebugEnabled());
+    }
+
+    /**
+     * configuration-path is given in the system-properties but the contents is bad
+     *
+     * @throws Exception
+     */
 	@Test
 	public void testEmptyFile() throws Exception {
-		System.setProperty(LOGGER_CONFIG_PROPERTY, "src/test/resources/empty.properties");
-		initializeLogger();
-		assertFalse(isLogggerInitialized());
-		System.err.println("Loggger was not initilaized from empty file");
+        clearSystemProperties();
+        final String path = "src/test/resources/empty.properties";
+        System.setProperty(LOGGER_CONFIG_PATH_PROPERTY, path);
+        assertFalse("Configuration  [" + path + "] is empty but the logger has been initialized", initializeLogger());
 	}
-	
-	@Test
-	public void testFile() throws Exception {
-		System.setProperty(LOGGER_CONFIG_PROPERTY, "src/test/resources/data/dbo2.log4j.properties");
-		initializeLogger();
-		assertTrue("Logger was not initialized", isLogggerInitialized());
-		log.info("Loggger initialized from  default resource");
-	}
-	
-	@Test
-	public void testDefaultResource() throws Exception {
-		System.getProperties().remove(LOGGER_CONFIG_PROPERTY);
-		initializeLogger();
-		assertTrue("Logger was not initialized", isLogggerInitialized());
-		log.info("Loggger initialized from  default resource");
-	}
-	
+
 	@Test
 	public void testCustomResource() throws Exception {
-		assertTrue("Initialization failed", initializeLogger("data/dbo2.log4j.properties"));
-		assertTrue("Logger was not initialized", isLogggerInitialized());
-		log.info("Loggger initialized from  custom resource");
+        clearSystemProperties();
+        final String resource = "data/dbo2.log4j.properties";
+        assertTrue("Logger was not initialized from custome resource [" + resource + "]", initializeLogger(resource));
+        log.debug("Loggger initialized from custome resource [" + resource + "] with DEBUG-priority for the logger");
+        assertTrue("Test resource-configuration has DEBUG-prioriry for the de.dbo-loggers but the logger has isDebugEnabled=false"
+                , log.isDebugEnabled());
 	}
-	
-	private boolean isLogggerInitialized() {
-		return  LogManager.getRootLogger().getAllAppenders().hasMoreElements();
-	}
-	
+
+    /**
+     * if outAndErrToLog is set, then any message should go to the logger.
+     * Test has be verified manually!
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testSystemErrorPrintToInitalizedLogger() throws Exception {
+        clearSystemProperties();
+        initializeLogger();
+        outAndErrToLog(true);
+        System.out.println("Message to System.out");
+        System.err.println("Message to System.err");
+        outAndErrToLog(false);
+    }
+
+    /**
+     * if outAndErrToLog is set, then printing exception stack trace should go to logger
+     * Test has be verified manually!
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDirectExceptionPrintStackTraceToInitalizedLogger() throws Exception {
+        clearSystemProperties();
+        initializeLogger();
+        outAndErrToLog(true);
+        new Exception("Test exception with printing the stack trace directly").printStackTrace();
+        outAndErrToLog(false);
+    }
+
+    @Test
+    public void ExceptionPrintStackTraceToInitalizedLogger() throws Exception {
+        clearSystemProperties();
+        initializeLogger();
+        outAndErrToLog(true);
+        try {
+            throw new Exception("Test exception");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            outAndErrToLog(false);
+        }
+
+    }
+
+    /**
+     * logger availability test.
+     * If the logger is reset, then it should not be available.
+     * On the other hand, if logger has been successfully initialized,
+     * the logger should be available
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testLoggerIsavailable() throws Exception {
+        clearSystemProperties();
+        LogManager.resetConfiguration();
+        assertFalse("Logger is available after reset", isAvailable());
+        if (initializeLogger()) {
+            assertTrue("Logger is not available after successfull initialization", isAvailable());
+        }
+    }
+
+    //
+    // HELPERS
+    //
+
+    private static final void clearSystemProperties() {
+        System.getProperties().remove(LOGGER_CONFIG_PATH_PROPERTY);
+        System.getProperties().remove(LOGGER_CONFIG_RESOURCE_PROPERTY);
+    }
 
 }
