04a346c7b75dbcbd6cad51e31c6439d505268593
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-10-11 19:21:52 +0200

    pom-collection works: test corrected

--- maven/pom/src/test/java/de/dbo/tools/maven/project/PomTest.java
+++ maven/pom/src/test/java/de/dbo/tools/maven/project/PomTest.java
@@ -9,13 +9,13 @@ public class PomTest  {
 
 	@Test
 	public void testPom() throws Exception {
-		final Pom pom = new Pom("D:/JAVA/WORKSPACES/ws/root.git/dependency-management/pom.xml");
+		final Pom pom = new Pom("pom.xml");
 		log.info("POM: " + pom.print());
 	}
 	
 	@Test
 	public void testPomCollection() throws Exception {
-		final String pattern = "D:/JAVA/WORKSPACES/ws/jbehave.git/**/pom.xml";
+		final String pattern = "../../**/pom.xml";
         final PomCollection pomCollection = PomCollection.newInstance(pattern);
 		log.info("POM Collection: " + pomCollection.print());
 	}
